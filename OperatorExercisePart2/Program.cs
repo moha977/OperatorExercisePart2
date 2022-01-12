94934440907ea01a85c44559057076beab24082e
Mohammad ahma <ahmad.moh0812@icloud.com>
2022-01-12 17:28:14 -0500

    Add project files.

---
+++ OperatorExercisePart2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercisePart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+            
+        {
+            AreaOfCircle();
+        }
+        public static void AreaOfCircle()
+        {
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = Math.PI * radius * radius;
+            Console.WriteLine(area);
+          
+        }
+    }
+}
